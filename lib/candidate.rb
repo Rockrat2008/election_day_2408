8b60b2ea79c3a27638c73724887dd4bf3e6fe508
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-12 13:45:56 -0400

    Initializes and counts votes - TESTS PASS

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,16 @@
 class Candidate
+    attr_reader :name,
+                :party,
+                :votes
+    attr_writer :votes
+
+    def initialize(candidate)
+        @name = candidate[:name]
+        @party = candidate[:party]
+        @votes = 0
+    end
+
+    def vote_for!
+        @votes += 1
+    end
 end
