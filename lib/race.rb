e2ba381cb256574dbb97dc5245750551f4dccf1b
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-12 16:47:15 -0400

    Creatss file with methods - TESTS PASS

--- lib/race.rb
+++ lib/race.rb
@@ -1,2 +1,18 @@
 class Race
+    attr_reader :office
+                :candidates
+    attr_writer :candidates
+
+    def initialize(race)
+        @office = race
+        @candidates = []
+    end
+
+    def candidates
+        @candidates
+    end
+
+    def register_candidate!(new_candidate)
+        @candidates << Candidate.new(new_candidate)
+    end
 end
